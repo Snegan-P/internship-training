6778fa6cd2157154ea6b99bafe9821cf1f34adcb
Snegan P <sneganp@gmail.com>
2026-06-26 17:31:36 +0530

    changes

---
+++ week 3/day15.sql/main.py
@@ -0,0 +1,36 @@
+from typing import List
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+
+from database import get_db
+import models, schemas
+
+app = FastAPI(title="Persistent Tasks API")
+
+
+@app.post("/tasks/", response_model=schemas.Task, status_code=status.HTTP_201_CREATED)
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    db_task = models.Task(
+        title=task.title, 
+        description=task.description, 
+        completed=task.completed
+    )
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)  # Gives us the generated ID from the database
+    return db_task
+
+
+@app.get("/tasks/", response_model=List[schemas.Task])
+def read_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    tasks = db.query(models.Task).offset(skip).limit(limit).all()
+    return tasks
+
+
+@app.get("/tasks/{task_id}", response_model=schemas.Task)
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    db_task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return db_task
